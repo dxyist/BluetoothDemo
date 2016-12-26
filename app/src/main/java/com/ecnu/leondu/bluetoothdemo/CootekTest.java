69067969a8627eb5675c7b283ce71fcf8aca13ee
xinyu.du <xinyu.du@cootek.cn>
2016-12-26 18:50:49 +0800

    6/17 Commit

---
+++ app/src/main/java/com/ecnu/leondu/bluetoothdemo/CootekTest.java
@@ -0,0 +1,12 @@
+package com.ecnu.leondu.bluetoothdemo;
+
+/**
+ * Created by LeonDu<xinyu.du@cootek.cn on 12/26/16.
+ */
+public class CootekTest {
+    String password = "1123123";
+    String passwd = "wjdiqjwdijq";
+    int pass = 123132;
+    String test = "pass:123123123";
+    String test2 = "pass==>123123123";
+}
