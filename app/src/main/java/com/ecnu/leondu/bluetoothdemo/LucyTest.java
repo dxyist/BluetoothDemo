3103ae7c94ee4c36a05246512df4c67b1a1bae9a
xinyu.du <xinyu.du@cootek.cn>
2016-12-26 22:32:59 +0800

    6/17 Commit

---
+++ app/src/main/java/com/ecnu/leondu/bluetoothdemo/LucyTest.java
@@ -0,0 +1,12 @@
+package com.ecnu.leondu.bluetoothdemo;
+
+/**
+ * Created by LeonDu@cootek.cn on 12/26/16.
+ */
+public class LucyTest {
+    private String  password    ="test";
+    private String passwd = "password";
+
+
+
+}
